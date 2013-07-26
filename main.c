8c6b7ef6bbb04d09116d2b95c70b8c19ed496dff
Pengu MC @ vito <michiel.van.der.coelen@gmail.com>
2013-07-26 14:01:46 +0200

    rc 1

--- main.c
+++ main.c
@@ -24,7 +24,7 @@
 #define NEXT_TRACK 2
 #define PREV_TRACK 3
 #define PAUSE_TRACK 4
-#define MAX_VOLUME 64
+#define MAX_VOLUME 63
 #define BUTTONPORT PINB
 #define BUTTON_PREV PB1
 #define BUTTON_PAUSE PB2
@@ -43,6 +43,7 @@ static uchar button_states = 0;
 uchar changed = 0;
 uchar adc_result = 0;
 uchar volume = MAX_VOLUME;
+uchar first_adc = 1;
 uint16_t normalizer = 0x0000;
 
 const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
@@ -68,8 +69,6 @@ const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
  
  uchar	usbFunctionSetup(uchar data[8]){
     usbRequest_t    *rq = (void *)data;
-    TOG(PORTB, PB0);
-
     usbMsgPtr = reportBuffer;
     if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
         if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
@@ -93,6 +92,7 @@ const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
 
 
 int main(){
+    SET(DDRC, PC5);//led
     //connect usb
     wdt_enable(WDTO_1S);
     usbInit();
@@ -105,12 +105,12 @@ int main(){
     usbDeviceConnect();
     sei();
     //setup ports, timers, and init variables.
-    SET(DDRD, PD7);//led
     ADMUX = (1<<REFS0) | (1<<ADLAR); //ref = avcc, left adjust result
     ADCSRA = (1<<ADEN) | (1<<ADSC) | 7; //turn on adc, prescale 128 -> 6.4 us conversion t
     DIDR0 = (1<<ADC0D); //digital input disabled for adc0 
     TCCR0B = 5; //prescale TC0 1024, 20 MHz -> 50 ns * 1024 * 255 = 13.056 ms
     reportBuffer[0] = 0x00;//(1<<1);
+    SET(PORTC, PC5);
     while(1){
         usbPoll();
         wdt_reset();
@@ -179,25 +179,28 @@ int main(){
                 }
             }
             //check adc
-            if(CHK(ADCSRA, ADIF)){
+            // if(CHK(ADCSRA, ADIF)){
+            if(!CHK(ADCSRA, ADSC)){
                 // normalizer = ADCH * MAX_VOLUME;
                 // adc_result = normalizer / 255;
-                adc_result = (ADCH >> 2);
+                adc_result = (ADCH>>2);
                 SET(ADCSRA, ADIF);
                 SET(ADCSRA, ADSC);
-                if(adc_result-1 > volume){
-                    SET(reportBuffer[0], VOLUME_UP);
-                    CLR(reportBuffer[0], VOLUME_DOWN);
-                    volume++;
-                    changed = 1;
-                }else if (adc_result+1 < volume){
-                    CLR(reportBuffer[0], VOLUME_UP);
-                    SET(reportBuffer[0], VOLUME_DOWN);
-                    volume--;
-                    changed = 1;
+                if(first_adc){
+                    volume = adc_result;
+                    first_adc = 0;
                 }else{
-                    CLR(reportBuffer[0], VOLUME_UP);
-                    CLR(reportBuffer[0], VOLUME_DOWN);
+                    if(changed == 0){
+                        if(adc_result+1 > volume){ //reversed because soldering.
+                            SET(reportBuffer[0], VOLUME_DOWN);
+                            if(volume < MAX_VOLUME) volume++;
+                            changed = 1;
+                        }else if (adc_result+1 < volume){
+                            SET(reportBuffer[0], VOLUME_UP);
+                            if(volume > 0) volume--;
+                            changed = 1;
+                        }
+                    }
                 }
             }
         }
@@ -205,7 +208,6 @@ int main(){
             changed = 0;
             TIFR0 = 1<<TOV0;
             TCNT0 = 0;
-            TOG(PORTD, PD7);
             wdt_reset();
             usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
             reportBuffer[0] = 0x00;
