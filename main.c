06f6407d085634bea8cde987152a620171722c8e
Pengu MC @ vito <michiel.van.der.coelen@gmail.com>
2013-07-31 16:04:47 +0200

    formatting + comments

--- main.c
+++ main.c
@@ -41,10 +41,9 @@ static uchar button_states = 0;
 #define STATE_PAUSE 4
 
 uchar changed = 0;
-uchar adc_result = 0;
+uchar adc_result = 0; 
 uchar volume = MAX_VOLUME;
 uchar first_adc = 1;
-uint16_t normalizer = 0x0000;
 
 const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
     0x05, 0x0C, // Usage Page (Consumer Devices)
@@ -80,7 +79,7 @@ const PROGMEM char usbHidReportDescriptor[31] = {   /* USB report descriptor */
         }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
             idleRate = rq->wValue.bytes[1];
         }
-    }else{
+    }else{ //some debug stuff that's left in.
         if(rq->bRequest == USBRQ_GET_LAST_ADC_RESULT){
             msgBuffer = adc_result;
             usbMsgPtr = &msgBuffer;
@@ -109,33 +108,33 @@ int main(){
     ADCSRA = (1<<ADEN) | (1<<ADSC) | 7; //turn on adc, prescale 128 -> 6.4 us conversion t
     DIDR0 = (1<<ADC0D); //digital input disabled for adc0 
     TCCR0B = 5; //prescale TC0 1024, 20 MHz -> 50 ns * 1024 * 255 = 13.056 ms
-    reportBuffer[0] = 0x00;//(1<<1);
-    SET(PORTC, PC5);
+    reportBuffer[0] = 0x00; //usb report buffer
+    SET(PORTC, PC5); //status led on
     while(1){
-        usbPoll();
-        wdt_reset();
-        if(CHK(TIFR0, TOV0)){
-            TIFR0 = 1<<TOV0;
-            TCNT0 = 0;
+        usbPoll(); //check for usb communications
+        wdt_reset(); //reset watchdog timer
+        if(CHK(TIFR0, TOV0)){ //check if 13 ms have passed already
+            TIFR0 = 1<<TOV0;    //clear timer overflow flag
+            TCNT0 = 0;          //reset timer
             //check button states
             //NEXT 
             if(CHK(BUTTONPORT, BUTTON_NEXT)){           //if it's down
-                SET(button_states, STATE_NEXT);         //set current state to down
-                if(CHK(button_states, (STATE_NEXT+1))){ //if previous state was down
-                                                        //  do nothing
-                }else{                                  //else
-                    SET(reportBuffer[0], NEXT_TRACK);   //  send down state
-                    SET(button_states, (STATE_NEXT+1)); //  update previous state
-                    changed = 1;
+                SET(button_states, STATE_NEXT);           //set current state to down
+                if(CHK(button_states, (STATE_NEXT+1))){   //if previous state was down
+                                                            //nothing changed, do nothing
+                }else{                                    //else
+                    SET(reportBuffer[0], NEXT_TRACK);       //send down state
+                    SET(button_states, (STATE_NEXT+1));     //update previous state
+                    changed = 1;                            //set flag to send report
                 }
             }else{                                      //button is up
-                CLR(button_states, STATE_NEXT);         //set current state to up
-                if(CHK(button_states, (STATE_NEXT+1))){ //if previous was down
-                    CLR(reportBuffer[0], NEXT_TRACK);   //  send up state
-                    CLR(button_states, (STATE_NEXT+1)); //  update previous state
-                    changed = 1;
-                }else{                                  //if button already was up
-                                                        //  do nothing
+                CLR(button_states, STATE_NEXT);           //set current state to up
+                if(CHK(button_states, (STATE_NEXT+1))){   //if previous was down
+                    CLR(reportBuffer[0], NEXT_TRACK);       //send up state
+                    CLR(button_states, (STATE_NEXT+1));     //update previous state
+                    changed = 1;                            //set flag to send report
+                }else{                                    //else button already was up
+                                                            //nothing changed, do nothing
                 }
             }
             // PREVIOUS
@@ -179,39 +178,37 @@ int main(){
                 }
             }
             //check adc
-            // if(CHK(ADCSRA, ADIF)){
-            if(!CHK(ADCSRA, ADSC)){
-                // normalizer = ADCH * MAX_VOLUME;
-                // adc_result = normalizer / 255;
-                adc_result = (ADCH>>2);
-                SET(ADCSRA, ADIF);
-                SET(ADCSRA, ADSC);
-                if(first_adc){
-                    volume = adc_result;
-                    first_adc = 0;
+            if(!CHK(ADCSRA, ADSC)){ //if a conversion is done.
+                adc_result = (ADCH>>2); //store the result (discard some accuray to reduce jitter)
+                SET(ADCSRA, ADIF); //clear the conversion done flag
+                SET(ADCSRA, ADSC); //start the next conversion
+                if(first_adc){  //if this is the first adc we do since startup
+                    volume = adc_result; //sync volume and adc_result
+                    first_adc = 0; //next time won't be the first.
                 }else{
-                    if(changed == 0){
+                    if(changed == 0){ //if we're not already going to send other button presses
                         if(adc_result > volume){ //reversed because soldering.
-                            SET(reportBuffer[0], VOLUME_DOWN);
-                            if(volume < MAX_VOLUME) volume++;
-                            changed = 1;
-                        }else if (adc_result < volume){
-                            SET(reportBuffer[0], VOLUME_UP);
-                            if(volume > 0) volume--;
-                            changed = 1;
+                            SET(reportBuffer[0], VOLUME_DOWN);  //send volume down
+                            if(volume < MAX_VOLUME) volume++;   //increase our local volume as well
+                            changed = 1;                        //set flag to send report
+                        }else if (adc_result < volume){       //volume going down
+                            SET(reportBuffer[0], VOLUME_UP);    //send volume down
+                            if(volume > 0) volume--;            //decrease our local volume as well
+                            changed = 1;                        //set flag to send report
                         }
                     }
                 }
             }
         }
-        if(changed){
+        if(changed){ //something changed, send a report
             changed = 0;
             TIFR0 = 1<<TOV0;
             TCNT0 = 0;
             wdt_reset();
-            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
-            while(!usbInterruptIsReady()){}
-            reportBuffer[0] = 0x00;
+            usbSetInterrupt(reportBuffer, sizeof(reportBuffer)); //send report
+            while(!usbInterruptIsReady()){} //wait till it's sent.
+            //immediately sent a report that all buttons are up.
+            reportBuffer[0] = 0x00; 
             usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
         }
     } //while
